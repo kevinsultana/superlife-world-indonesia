2ddd458e46b593719574c5d35d9f245611c6e684
kevinsultana <kevinpdg.ks@gmail.com>
2025-02-27 21:02:42 +0700

    phase 1

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,36 +1,56 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./Header.module.css";
 import { FaChevronDown } from "react-icons/fa";
 
-export default function Header() {
+export default function Header(props) {
+  const { style } = props;
+  const [show, setShow] = useState(false);
   return (
-    <header className="flex-1 bg-white py-3">
+    <div className="flex-1 bg-white py-3 fixed w-full ">
       <div className="flex items-center justify-between px-20">
         <img
           src="https://superlifebioindonesia.com/wp-content/uploads/2023/07/cropped-logo-sbi.png"
           width={80}
           height={80}
         />
+        <div>
+          <a>
+            <h1>
+              <p>
+                <img />
+              </p>
+            </h1>
+          </a>
+        </div>
         <ul className="flex flex-1 mx-3 gap-6 text-black px-3 justify-end">
           <li className="flex items-center gap-2 overflow-auto hover:">
-            <a href="/#">Tentang Kami</a>
-            <span>
+            <a href="/#" onClick={() => setShow(!show)}>
+              Tentang Kami
+            </a>
+            <span onClick={() => setShow(!show)}>
               <FaChevronDown size={10} color="black" />
             </span>
-            <ul className="absolute top-16  bg-white">
-              <li className="flex items-center gap-2 underline">
-                <a href="/about">Sejarah</a>
-              </li>
-              <li className="flex items-center gap-2">
-                <a href="/about">Visi dan Misi</a>
-              </li>
-              <li className="flex items-center gap-2">
-                <a href="/about">Struktur Organisasi</a>
-              </li>
-              <li className="flex items-center gap-2">
-                <a href="/about">Penghargaan</a>
-              </li>
-            </ul>
+            {show && (
+              <ul className="absolute top-24 bg-white">
+                <li className="">
+                  <a href="/history">
+                    Sejarah
+                    {/* <span className="text-red-700 underline font-bold ">rah</span> */}
+                    {/* <strong>coba</strong> */}
+                  </a>
+                </li>
+                <li className="">
+                  <a href="/visi-dan-misi">Visi dan Misi</a>
+                </li>
+                <li className="">
+                  <a href="/struktur">Struktur Organisasi</a>
+                </li>
+                <li className="">
+                  <a href="/penghargaan">Penghargaan</a>
+                </li>
+              </ul>
+            )}
           </li>
           <li className="flex items-center gap-2">
             <a href="/product">Produk</a>
@@ -63,6 +83,6 @@ export default function Header() {
           </li>
         </ul>
       </div>
-    </header>
+    </div>
   );
 }
